b5ab88de1a7f53b27f31de9abd26e8200c92c43c
artem <artemfr@hotmail.com>
2023-01-08 19:08:18 +0100

    added port normalization

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,16 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     res.status(201);
+    next();
 })
 
 app.use((req, res, next) => {
     res.json({message: 'Your request was successful!'});
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Response sent successfully!');
 });
 
 module.exports = app;
