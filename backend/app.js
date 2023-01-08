a48728c136d66ad03a4a9304d0741f661b7f4646
artem <artemfr@hotmail.com>
2023-01-08 18:38:59 +0100

    learning back end, creating server

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+   console.log('Request received!'); 
+   next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+})
+
+app.use((req, res, next) => {
+    res.json({message: 'Your request was successful!'});
+});
+
+module.exports = app;
