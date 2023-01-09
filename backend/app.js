606545e37f8a3c48e4e6688eb2e2fae8b71133f3
artem <artemfr@hotmail.com>
2023-01-09 01:37:35 +0100

    changing app.js to show the stuff on the site

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-   console.log('Request received!'); 
-   next();
-});
-
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-})
-
-app.use((req, res, next) => {
-    res.json({message: 'Your request was successful!'});
-    next();
-});
-
-app.use((req, res, next) => {
-    console.log('Response sent successfully!');
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'dbhsdh',
+            title: 'My first thing',
+            description: 'All of the info about my first thing',
+            imageUrl: '',
+            price: 4900,
+            userId: 'dfgfdgdfg',
+        }, {
+            _id: 'dbhsdhfhrt',
+            title: 'My 2nd thing',
+            description: 'All of the info about my 2nd thing',
+            imageUrl: '',
+            price: 2900,
+            userId: 'dfgfdgdfg',
+        },
+    ];
+    res.status(200).json(stuff);
 });
 
 module.exports = app;
